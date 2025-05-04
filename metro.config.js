fca197caff60146c1fe0f688698cfe543fb15632
AbdiazizM3 <abdiazizmohamed333555@gmail.com>
2025-05-05 00:55:04 +0100

    Reverted metro.config.js changes

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,28 @@ const { getDefaultConfig } = require("@react-native/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+// Add support for .cjs files
+defaultConfig.resolver.assetExts.push("cjs");
+
+// Add better error handling
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  minifierConfig: {
+    keep_classnames: true,
+    keep_fnames: true,
+    mangle: {
+      keep_classnames: true,
+      keep_fnames: true,
+    },
+  },
+};
+
+// Add better source map support
+defaultConfig.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+
 module.exports = defaultConfig;
